df1cc92a6c7fd080542a2859bb7af5c8d43b79e5
dobredani <dobre.dani.gtalk@gmail.com>
2020-03-24 18:34:16 +0200

    Slide-uri + mici modificari

---
+++ src/MainFrame.java
@@ -0,0 +1,30 @@
+import javax.swing.*;
+import java.awt.*;
+
+
+public class MainFrame extends JFrame {
+    ConfigPanel configPanel;
+    ControlPanel controlPanel;
+    DrawingPanel canvas;
+
+    public MainFrame() {
+        super("My Drawing Application");
+        init();
+    }
+
+    private void init() {
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        //create the components
+        canvas = new DrawingPanel(this);
+ //...TODO
+
+        //arrange the components in the container (frame)
+        //JFrame uses a BorderLayout by default
+        add(canvas, BorderLayout.CENTER); //this is BorderLayout.CENTER
+ //...TODO
+
+        //invoke the layout manager
+        pack();
+    }
+}
